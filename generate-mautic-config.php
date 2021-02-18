6a4c07c649fd16ce6948c62d2836397d8fdae61f
Jeremy Vernon <jeremy.vernon@gov.bc.ca>
2021-02-17 16:30:27 -0800

    Altered local-template to reflect correct naming of variables.

--- generate-mautic-config.php
+++ generate-mautic-config.php
@@ -2,16 +2,16 @@
 
 //VARIABLES
 $configVars = array (
-    'INSTANCE'                      => '', //this should indicate the client or client-project this app belongs to (e.g. ihub-f4c comms-compass)
-    'NAMESPACE'                     => '', //this must be one of the four provisioned namespaces, but this is not enforced here.
-    'CLUSTER_IP'                    => '', //this must be a number between 0-255 and be unique to this namespace (?maybe to the whole cluster?)
-    'DB_CLUSTER_IP'                 => '', //this must be a number between 0-255 and be unique to this namespace, and differemt from CLUSTER_IP
+    'INSTANCE'                      => 'BIG-internal', //this should indicate the client or client-project this app belongs to (e.g. ihub-f4c comms-compass). Must be lowercase alphanumeric or '-' characters (unenforced).
+    'NAMESPACE'                     => 'b07dd3-prod', //this must be one of the four provisioned namespaces, but this is not enforced here.
+    'CLUSTER_IP'                    => '175', //this must be a number between 0-255 and be unique to this namespace (?maybe to the whole cluster?)
+    'DB_CLUSTER_IP'                 => '176', //this must be a number between 0-255 and be unique to this namespace, and differemt from CLUSTER_IP
     'DATABASE_PASSWORD'             => generateStrongPassword(32),  //currently the DB secret must be created manually from the values input here - due to character encoding blah blah.
     'DATABASE_USER'                 => 'user' . generateStrongPassword(5),
     'DATABASE_ROOT_PASSWORD'        => generateStrongPassword(32),
     'DATABASE_NAME'                 => 'mautic',
     'HASH_KEY'                      => '7bdc46b5a52f8725dd834dc8f125ff52452c9b08129553d6785aa5869383a9c5', //TODO: write function to generate this value.
-    'MAILER_FROM_NAME'              => '', //This should be the name of the client org - it's overridable in the OCP4 admin.
+    'MAILER_FROM_NAME'              => 'Behavioural Insights Group', //This should be the name of the client org - it's overridable in the OCP4 admin.
     'MAILER_FROM_EMAIL'             => 'no-reply@gov.bc.ca', //The default is no-reply@gov.bc.ca
     'MAILER_ENCRYPTION'             => '', //default is null
     'MAILER_USER'                   => '', //in case you wish to use an external mailer with username/password etc.
